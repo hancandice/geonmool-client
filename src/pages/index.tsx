00fbf26f403b1dbd41c7412500fa5a898d4a68f5
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2020-11-16 19:00:59 +0900

    Chakra Styling for index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@chakra-ui/core";
+import { Box, Heading, Link, Stack, Text } from "@chakra-ui/core";
 import { withUrqlClient } from "next-urql";
 import React from "react";
 import { Layout } from "../components/Layout";
@@ -7,7 +7,11 @@ import { createUrqlClient } from "../utils/createUrqlClient";
 import NextLink from "next/link";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 30,
+    },
+  });
   return (
     <Layout>
       <NextLink href="/create-post">
@@ -18,7 +22,14 @@ const Index = () => {
       {!data ? (
         <div>loading...</div>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={8}>
+          {data.posts.map((p) => (
+            <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+              <Heading fontSize="xl">{p.title}</Heading>
+              <Text mt={4}>{p.text.slice(0, 50)}</Text>
+            </Box>
+          ))}
+        </Stack>
       )}
     </Layout>
   );
